50c012dde8a0d08f8baf326f813c1f290334441f
shaan-m0 <shaan@m0.ventures>
2024-09-18 20:53:40 +0530

    added type safe env vars

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import env from './env'
 
 const app = express();
-const PORT = 5000;
+const PORT = env.PORT ||  5000;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
---
+++ env.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const schema = z.object({
+  MONGO_URI: z.string(),
+  PORT: z.coerce.number(),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+});
+
+const parsed = schema.safeParse(process.env);
+
+if (!parsed.success) {
+  console.error(
+    "❌ Invalid environment variables:",
+    JSON.stringify(parsed.error.format(), null, 4)
+  );
+  process.exit(1);
+}
+
+export default parsed.data;
